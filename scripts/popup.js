c4e2efebfcf894e02ca11987fa11872187c95b35
= <giovannie.marrero@upr.edu>
2023-11-29 21:26:23 -0400

    Se quedo asi

--- scripts/popup.js
+++ scripts/popup.js
@@ -14,102 +14,6 @@ function hidePopup() {
     popup.style.display = 'none';
 }
 
-function setGender(gender){
-    if (gender == "M")
-        gender_M = 1;
-}
-
-function setProgramCode(prog_code) {
-    const programCodes = {
-        "BIOG": "prog_code_BIOG",
-        "CCOM": "prog_code_CCOM",
-        "CONT": "prog_code_CONT",
-        "EFEE": "prog_code_EFEE",
-        "ESCO": "prog_code_ESCO",
-        "FINA": "prog_code_FINA",
-        "GEMA": "prog_code_GEMA",
-        "GERE": "prog_code_GERE",
-        "MERC": "prog_code_MERC",
-        "SICI": "prog_code_SICI",
-        "SINF": "prog_code_SINF",
-        "SOFI": "prog_code_SOFI",
-        "TIEL": "prog_code_TIEL"
-    };
-
-    if (programCodes.hasOwnProperty(prog_code)) {
-        window[programCodes[prog_code]] = 1;
-    }
-}
-
-function setTownOrigin(town_origin) {
-    const townOrigins = {
-        "ARECIBO": "town_origin_ARECIBO",
-        "ARROYO": "town_origin_ARROYO",
-        "BARCELONETA": "town_origin_BARCELONETA",
-        "BARRANQUITAS": "town_origin_BARRANQUITAS",
-        "BAYAMON": "town_origin_BAYAMON",
-        "CAGUAS": "town_origin_CAGUAS",
-        "CAMUY": "town_origin_CAMUY",
-        "CANOVANAS": "town_origin_CANOVANAS",
-        "CAROLINA": "town_origin_CAROLINA",
-        "CATANO": "town_origin_CATANO",
-        "CAYEY": "town_origin_CAYEY",
-        "CIALES": "town_origin_CIALES",
-        "CIDRA": "town_origin_CIDRA",
-        "COMERIO": "town_origin_COMERIO",
-        "COROZAL": "town_origin_COROZAL",
-        "DORADO": "town_origin_DORADO",
-        "FAJARDO": "town_origin_FAJARDO",
-        "GUAYAMA": "town_origin_GUAYAMA",
-        "GUAYNABO": "town_origin_GUAYNABO",
-        "GURABO": "town_origin_GURABO",
-        "HATILLO": "town_origin_HATILLO",
-        "HUMACAO": "town_origin_HUMACAO",
-        "ISABELA": "town_origin_ISABELA",
-        "JUNCOS": "town_origin_JUNCOS",
-        "LAS_PIEDRAS": "town_origin_LAS_PIEDRAS",
-        "LOIZA": "town_origin_LOIZA",
-        "LUQUILLO": "town_origin_LUQUILLO",
-        "MANATI": "town_origin_MANATI",
-        "MOROVIS": "town_origin_MOROVIS",
-        "NAGUABO": "town_origin_NAGUABO",
-        "NARANJITO": "town_origin_NARANJITO",
-        "OROCOVIS": "town_origin_OROCOVIS",
-        "PONCE": "town_origin_PONCE",
-        "RIO_GRANDE": "town_origin_RIO_GRANDE",
-        "SABANA_GRANDE": "town_origin_SABANA_GRANDE",
-        "SAN_JUAN": "town_origin_SAN_JUAN",
-        "SAN_LORENZO": "town_origin_SAN_LORENZO",
-        "TOA_ALTA": "town_origin_TOA_ALTA",
-        "TOA_BAJA": "town_origin_TOA_BAJA",
-        "TRUJILLO_ALTO": "town_origin_TRUJILLO_ALTO",
-        "VEGA_ALTA": "town_origin_VEGA_ALTA",
-        "VEGA_BAJA": "town_origin_VEGA_BAJA",
-        "YABUCOA": "town_origin_YABUCOA",
-        "YAUCO": "town_origin_YAUCO"
-    };
-
-    if (townOrigins.hasOwnProperty(town_origin)) {
-        window[townOrigins[town_origin]] = 1;
-    }
-}
-
-function setYearCred(year_cred){
-    const yearCredMappings = {
-        "6TO": "year_cred_6TO",
-        "CNB": "year_cred_CNB",
-        "CUA": "year_cred_CUA",
-        "SEG": "year_cred_SEG",
-        "SNB": "year_cred_SNB",
-        "TER": "year_cred_TER",
-        "TNB": "year_cred_TNB"
-    };
-
-    if (yearCredMappings.hasOwnProperty(year_cred)) {
-        window[yearCredMappings[year_cred]] = 1;
-    }
-}
-
 function logisticFunction(gender, prog_code, gpa, town_origin, year_cred){
     var gender_M=0;
     var prog_code_BIOG=0;
@@ -178,11 +82,139 @@ function logisticFunction(gender, prog_code, gpa, town_origin, year_cred){
     var year_cred_TER=0;
     var year_cred_TNB=0;
 
-    setGender(gender);
-    setProgramCode(prog_code);
-    setTownOrigin(town_origin);
-    setYearCred(year_cred);
+    if (gender == "M")
+        gender_M = 1;
     
+    if (prog_code == "BIOG")
+        prog_code_BIOG = 1;
+    else if (prog_code == "CCOM")
+        prog_code_CCOM = 1;
+    else if (prog_code == "CONT")
+        prog_code_CONT = 1;
+    else if (prog_code == "EFEE")
+        prog_code_EFEE = 1;
+    else if (prog_code == "ESCO")
+        prog_code_ESCO = 1;
+    else if (prog_code == "FINA")
+        prog_code_FINA = 1;
+    else if (prog_code == "GEMA")
+        prog_code_GEMA = 1;
+    else if (prog_code == "GERE")
+        prog_code_GERE = 1;
+    else if (prog_code == "MERC")
+        prog_code_MERC = 1;
+    else if (prog_code == "SICI")
+        prog_code_SICI = 1;
+    else if (prog_code == "SINF")
+        prog_code_SINF = 1;
+    else if (prog_code == "SOFI")
+        prog_code_SOFI = 1;
+    else if (prog_code == "TIEL")
+        prog_code_TIEL = 1;
+    
+    if (town_origin == "ARECIBO")
+        town_origin_ARECIBO = 1;
+    else if (town_origin == "ARROYO")
+        town_origin_ARROYO = 1;
+    else if (town_origin == "BARCELONETA")
+        town_origin_BARCELONETA = 1;
+    else if (town_origin == "BARRANQUITAS")
+        town_origin_BARRANQUITAS = 1;
+    else if (town_origin == "BAYAMON")
+        town_origin_BAYAMON = 1;
+    else if (town_origin == "CAGUAS")
+        town_origin_CAGUAS = 1;
+    else if (town_origin == "CAMUY")
+        town_origin_CAMUY = 1;
+    else if (town_origin == "CANOVANAS")
+        town_origin_CANOVANAS = 1;
+    else if (town_origin == "CAROLINA")
+        town_origin_CAROLINA = 1;
+    else if (town_origin == "CATANO")
+        town_origin_CATANO = 1;
+    else if (town_origin == "CAYEY")
+        town_origin_CAYEY = 1;
+    else if (town_origin == "CIALES")
+        town_origin_CIALES = 1;
+    else if (town_origin == "CIDRA")
+        town_origin_CIDRA = 1;
+    else if (town_origin == "COMERIO")
+        town_origin_COMERIO = 1;
+    else if (town_origin == "COROZAL")
+        town_origin_COROZAL = 1;
+    else if (town_origin == "DORADO")
+        town_origin_DORADO = 1;
+    else if (town_origin == "FAJARDO")
+        town_origin_FAJARDO = 1;
+    else if (town_origin == "GUAYAMA")
+        town_origin_GUAYAMA = 1;
+    else if (town_origin == "GUAYNABO")
+        town_origin_GUAYNABO = 1;
+    else if (town_origin == "GURABO")
+        town_origin_GURABO = 1;
+    else if (town_origin == "HATILLO")
+        town_origin_HATILLO = 1;
+    else if (town_origin == "HUMACAO")
+        town_origin_HUMACAO = 1;
+    else if (town_origin == "ISABELA")
+        town_origin_ISABELA = 1;
+    else if (town_origin == "JUNCOS")
+        town_origin_JUNCOS = 1;
+    else if (town_origin == "LAS_PIEDRAS")
+        town_origin_LAS_PIEDRAS = 1;
+    else if (town_origin == "LOIZA")
+        town_origin_LOIZA = 1;
+    else if (town_origin == "LUQUILLO")
+        town_origin_LUQUILLO = 1;
+    else if (town_origin == "MANATI")
+        town_origin_MANATI = 1;
+    else if (town_origin == "MOROVIS")
+        town_origin_MOROVIS = 1;
+    else if (town_origin == "NAGUABO")
+        town_origin_NAGUABO = 1;
+    else if (town_origin == "NARANJITO")
+        town_origin_NARANJITO = 1;
+    else if (town_origin == "OROCOVIS")
+        town_origin_OROCOVIS = 1;
+    else if (town_origin == "PONCE")
+        town_origin_PONCE = 1;
+    else if (town_origin == "RIO_GRANDE")
+        town_origin_RIO_GRANDE = 1;
+    else if (town_origin == "SABANA_GRANDE")
+        town_origin_SABANA_GRANDE = 1;
+    else if (town_origin == "SAN_JUAN")
+        town_origin_SAN_JUAN = 1;
+    else if (town_origin == "SAN_LORENZO")
+        town_origin_SAN_LORENZO = 1;
+    else if (town_origin == "TOA_ALTA")
+        town_origin_TOA_ALTA = 1;
+    else if (town_origin == "TOA_BAJA")
+        town_origin_TOA_BAJA = 1;
+    else if (town_origin == "TRUJILLO_ALTO")
+        town_origin_TRUJILLO_ALTO = 1;
+    else if (town_origin == "VEGA_ALTA")
+        town_origin_VEGA_ALTA = 1;
+    else if (town_origin == "VEGA_BAJA")
+        town_origin_VEGA_BAJA = 1;
+    else if (town_origin == "YABUCOA")
+        town_origin_YABUCOA = 1;
+    else if (town_origin == "YAUCO")
+        town_origin_YAUCO = 1;
+    
+    if(year_cred == "6TO")
+        year_cred_6TO = 1;
+    else if(year_cred == "CNB")
+        year_cred_CNB = 1;
+    else if(year_cred == "CUA")
+        year_cred_CUA = 1;
+    else if(year_cred == "SEG")
+        year_cred_SEG = 1;
+    else if(year_cred == "SNB")
+        year_cred_SNB = 1;
+    else if(year_cred == "TER")
+        year_cred_TER = 1;
+    else if(year_cred == "TNB")
+        year_cred_TNB = 1;
 
     try{
         return 1 / (1 + Math.E ** (-(0.06135034619497988 + 0.0888*gender_M + -0.0195*prog_code_BIOG + 0.0393*prog_code_CCOM 
